4d61b4ae28f3214fc8dd7ed3189f509e11f77ac8
Louis Orleans <dudeofawesome@gmail.com>
2013-06-18 14:07:36 -0700

    programming - Jeremy Louis
    f,li

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -1,5 +1,7 @@
 package org.teachingkidsprogramming.recipes;
 
+import java.awt.Color;
+
 import org.teachingextensions.logo.Tortoise;
 
 public class SimpleSquare
@@ -8,11 +10,16 @@ public class SimpleSquare
   {
     Tortoise.show();
     //  Make the tortoise move as fast as possible --#6
-    //  Do the following 4 times --#5.1
-    //      Change the color of the line the tortoise draws to "blue" --#4
-    //      Move the tortoise 50 pixels --#2
-    Tortoise.move(50);
-    //      Turn the tortoise to the right (90 degrees) --#3
-    //  Repeat --#5.2
+    moveMe();
+  }
+  public static void moveMe()
+  {
+    for (int i = 0; i < 4; i++)
+    {
+      Tortoise.setPenColor(Color.BLUE);
+      Tortoise.move(50);
+      Tortoise.turn(90);
+    }
+    moveMe();
   }
 }
