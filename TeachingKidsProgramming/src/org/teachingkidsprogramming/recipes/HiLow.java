2e819cae0b943eef156eb13deabb1af34c1fa6e3
Jeremy Stefun <jtstefun@hotmail.com>
2013-06-20 14:46:04 -0700

    HiLow Guessing Game--Matt/Jeremy
    iopqwjkjn

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/HiLow.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/HiLow.java
@@ -1,21 +1,33 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.utils.Sounds;
+import org.teachingextensions.windows.MessageBox;
+
 public class HiLow
 {
   public static void main(String[] args)
   {
     //    Choose a random number between 1 and 100 --#4.1 (fake!) & --#13
     //    Do the following 8 times --#9
-    //     Ask the user for a guess --#1
-    //     If the guess is correct --#4
-    //     Play a bell --#2
-    //     Tell the user that they won the game  --#3
-    //     and exit --#10
-    //     Otherwise, if the guess is too high --#6
-    //     Tell the end user that it is too high --#5
-    //     Otherwise, if the guess is too low --#8
-    //     Tell the end user that it is too low --#7
-    //    If after 8 times they haven't guessed correctly then --#12
-    //     Tell them they've lost the game --#11
+    int randomNumber = (int) Math.floor(Math.random() * 101);
+    for (int i = 0; i <= 8; i++)
+    {
+      int userGuess = MessageBox.askForNumericalInput("Guess a Number.");
+      if (userGuess == randomNumber)
+      {
+        Sounds.playBeep();
+        MessageBox.showMessage("Wow I can't believe you won. Man, they let anyone win these days...");
+        break;
+      }
+      else if (userGuess > randomNumber)
+      {
+        MessageBox.showMessage("YOUR GUESS WAS TOO HIGH!!!!! WHAT ARE YOU, CRAZY?");
+      }
+      else if (userGuess < randomNumber)
+      {
+        MessageBox.showMessage("YOUR GUESS WAS TOO LOW!!!! WEAKLING, HAVE STRONGER NUMBERS!!!");
+      }
+    }
+    MessageBox.showMessage("FAILURE!!! YOUR PARENTS WILL NEVER LOVE YOU!");
   }
 }
